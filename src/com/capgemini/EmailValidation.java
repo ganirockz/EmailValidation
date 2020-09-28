ef96bfded01d54f3fd3d9a5c9770ea8325704a20
ganirockz <ganeshrapeti8@gmail.com>
2020-09-28 15:02:30 +0530

    [Ganesh]Added: Ensure @ and bridgelabZ in Email

--- src/com/capgemini/EmailValidation.java
+++ src/com/capgemini/EmailValidation.java
@@ -7,14 +7,23 @@ import java.util.regex.Pattern;
 public class EmailValidation {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Pattern pattern = Pattern.compile("^([a][b][c])+");
-		System.out.println("Enter the EMail address to check pattern :");
-		String input = sc.nextLine();
-		Matcher m = pattern.matcher(input);
-		if (m.find()) {
-			System.out.println("Valid");
-		} else {
-			System.out.println("Not Valid");
+		Pattern pattern = Pattern.compile("^([a][b][c])+[@]([b][r][i][d][g][e][l][a][b][z])+");
+		while (true) {
+			System.out.println("Do you want to enter Email address: \nselect \n1.yes\n2.no");
+			int option = Integer.parseInt(sc.nextLine());
+			if(option == 1) {
+			System.out.println("Enter the EMail address to check pattern :");
+			String input = sc.nextLine();
+			Matcher m = pattern.matcher(input);
+			if (m.find()) {
+				System.out.println("Valid");
+			} else {
+				System.out.println("Not Valid");
+			}
+			}
+			else{
+				break;
+			}
 		}
 		sc.close();
 	}
